40dd491bd6839ec6e4a631b5dc70dabb90c69f22
Matheus Migge <matheusmigge@gmail.com>
2025-04-01 21:23:18 -0300

    folder organization

---
+++ frontend/frontend/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-L3RR2ZJB.js";
+import "./chunk-IUD5A5RE.js";
+import "./chunk-ET3UHB5R.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
