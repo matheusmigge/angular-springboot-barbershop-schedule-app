1b09accb250e8332772304c115d0598b7d4adccc
Matheus Migge <matheusmigge@gmail.com>
2025-04-02 20:59:33 -0300

    add client selection to schedule component and fetch clients from JSON

--- frontend/src/app/components/schedule/schedule.component.ts
+++ frontend/src/app/components/schedule/schedule.component.ts
@@ -9,16 +9,20 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormControl, FormsModule, Validators } from '@angular/forms';
-import {MatTimepickerModule} from '@angular/material/timepicker';
+import { MatTimepickerModule } from '@angular/material/timepicker';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
-interface Food {
-  value: string;
-  viewValue: string;
+export interface Client {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
 }
 
 @Component({
   selector: 'app-schedule',
-  imports: [BannerComponent, TableComponent, MatCardModule, MatDatepickerModule, FormsModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatTimepickerModule,MatButtonModule],
+  imports: [CommonModule, BannerComponent, TableComponent, MatCardModule, MatDatepickerModule, FormsModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatTimepickerModule, MatButtonModule],
   templateUrl: './schedule.component.html',
   styleUrl: './schedule.component.css',
   providers: [provideNativeDateAdapter()],
@@ -26,5 +30,15 @@ interface Food {
 })
 export class ScheduleComponent {
   selected = model<Date | null>(null);
-  selectedValue: string = "";
+  clients: Client[] = [];
+  selectedValue!: string;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<Client[]>('/assets/data/clients.json').subscribe(data => {
+      this.clients = data;
+    });
+  }
 }
