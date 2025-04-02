06cd01b14488bc3bb87cf25b60919eace0f7efbc
Matheus Migge <matheusmigge@gmail.com>
2025-04-02 15:51:48 -0300

    add clients list component with banner and update app layout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { NewClientComponent } from "./components/new-client/new-client.component";
+import { ClientsListComponent } from "./components/clients-list/clients-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, NewClientComponent],
+  imports: [RouterOutlet, HeaderComponent, NewClientComponent, ClientsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/src/app/components/clients-list/clients-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BannerComponent } from "../banner/banner.component";
+
+@Component({
+  selector: 'app-clients-list',
+  imports: [BannerComponent],
+  templateUrl: './clients-list.component.html',
+  styleUrl: './clients-list.component.css'
+})
+export class ClientsListComponent {
+
+}
