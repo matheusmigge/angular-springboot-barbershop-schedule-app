2c52b7894e354823a23f65c71562e71cb2daf48b
Matheus Migge <matheusmigge@gmail.com>
2025-04-03 15:52:27 -0300

    feat: initialize API with basic application structure and properties

---
+++ backend/src/main/java/bootcamp_dio/barbershop_schedule_api/BarbershopScheduleApiApplication.java
@@ -0,0 +1,13 @@
+package bootcamp_dio.barbershop_schedule_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopScheduleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopScheduleApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/bootcamp_dio/barbershop_schedule_api/BarbershopScheduleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp_dio.barbershop_schedule_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopScheduleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
