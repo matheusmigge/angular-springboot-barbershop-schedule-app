54126a46f79068a9d164a0ed09a250523224c954
Matheus Migge <matheusmigge@gmail.com>
2025-04-02 21:31:17 -0300

    refactor: remove hardcoded client data and add schedules table component

--- frontend/src/app/components/clients-table/clients-table.component.ts
+++ frontend/src/app/components/clients-table/clients-table.component.ts
@@ -31,14 +31,6 @@ export class TableComponent implements AfterViewInit {
   @ViewChild(MatSort)
   sort!: MatSort;
 
-  private clients: Client[] = [
-    { id: 1, name: 'John Doe', email: 'john.doe@example.com', phone: '123-456-7890' },
-    { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com', phone: '987-654-3210' },
-    { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com', phone: '555-123-4567' },
-    { id: 4, name: 'Bob Brown', email: 'bob.brown@example.com', phone: '444-987-6543' },
-    { id: 5, name: 'Charlie Davis', email: 'charlie.davis@example.com', phone: '333-222-1111' },
-  ];
-
   constructor(private http: HttpClient) {
 
     this.dataSource = new MatTableDataSource<Client>([]);
---
+++ frontend/src/app/components/schedules-table/schedules-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedules-table',
+  imports: [],
+  templateUrl: './schedules-table.component.html',
+  styleUrl: './schedules-table.component.css'
+})
+export class SchedulesTableComponent {
+
+}
