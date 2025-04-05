21f8ade6f4560c230148a7832a4e0544ec485147
Matheus Migge <matheusmigge@gmail.com>
2025-04-04 23:31:12 -0300

    fix: correct capitalization in client edit and registration header

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,9 +4,9 @@ import { ClientsComponent } from './pages/clients/clients.component';
 import { ScheduleComponent } from './pages/schedule/schedule.component';
 
 export const routes: Routes = [
-    { path: 'clients/new-client', component: NewClientComponent, data: { title: 'Cadastrar Cliente' } },
-    { path: 'new-client/:id', component: NewClientComponent, data: { title: 'Editar Cliente' }  },
-    { path: 'clients/list', component: ClientsComponent, data: { title: 'Clientes Cadastrados' } },
+    { path: 'clients/new-client', component: NewClientComponent, data: { title: 'Cadastrar cliente' } },
+    { path: 'new-client/:id', component: NewClientComponent, data: { title: 'Editar cliente' }  },
+    { path: 'clients/list', component: ClientsComponent, data: { title: 'Clientes cadastrados' } },
     { path: 'schedules', component: ScheduleComponent, data: { title: 'Agendamentos' } },
     { path: '**', redirectTo: 'schedules' }
 ];
